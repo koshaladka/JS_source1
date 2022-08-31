c74d5327a59044c9497bd53acdf94fe47dac51e0
Alena Work <123@123.ru>
2022-08-31 09:33:01 +0300

    31 08 1.9

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -2,7 +2,8 @@ const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector) {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+              close = document.querySelector(closeSelector),
+              scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -12,7 +13,11 @@ const modals = () => {
     
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
+              
+
                 // document.body.classList.add('modal-open');
+
             });
         });
 
@@ -26,17 +31,32 @@ const modals = () => {
             if (e.target === modal) {
                 modal.style.display = "none";
                 document.body.style.overflow = ""; 
+                document.body.style.marginRight = '0px';
                 // document.body.classList.remove('modal-open');
             }
         });
     }
 
-    //показ моадльного окна спустя вермя
+    //показ модального окна спустя вермя
     function showModalByTime(selector, time) {
         setTimeout(function() {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = "hidden";
         }, time);
+    }
+
+    function calcScroll () {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
+
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
