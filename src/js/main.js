a255c259293bb3a0427bc04e4c79d8a83de7da7b
Alena Work <123@123.ru>
2022-10-03 18:05:48 +0300

    form 33 min

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,20 @@
 import "./slider";
 import modals from './modules/modals';
 import tabs from './modules/tabs';
-import forms from './modules/forms'
+import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
 
-console.log('1');
+
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
+
+    let modalState = {};
+
+    changeModalState(modalState);
     modals();
     tabs ('.glazing_slider', '.glazing_block', '.glazing_content', 'active', 'flex');
     tabs ('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click', 'flex');
     tabs ('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms();
+    forms(modalState);
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,12 @@
-const forms = () => {
-    const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumImputs from "./checkNumInputs";
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+          
+
+    checkNumImputs('input[name="user_phone"]');
+  
     
     const message = {
         loading: 'Загрузка...',
@@ -40,7 +39,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
-            console.log(formData);
+            if (item.getAttribute('data-calc') =="end") {
+                for(let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
